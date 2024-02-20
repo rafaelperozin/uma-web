75f7c88804d3134b5b3fd52d4794f3bda0f4c8ba
Rafael Perozin <rafaelperozin@outlook.com>
2024-02-20 19:25:33 +0100

    build(persist): save store data in local storage

--- src/constexts/store.context.tsx
+++ src/constexts/store.context.tsx
@@ -1,6 +1,7 @@
-import {ReactNode, useContext} from 'react';
+import {ReactNode, useContext, useEffect, useState} from 'react';
 import {createContext} from 'react';
 import { RootStoreType, rootStore } from '../stores/root.store';
+import persist from 'mst-persist';
 
 const StoreContext = createContext<RootStoreType | undefined>(undefined);
 
@@ -8,7 +9,31 @@ export interface StoreProviderProps {
   children: ReactNode;
 }
 
-export const StoreProvider = ({ children }: StoreProviderProps) =>
-  <StoreContext.Provider value={rootStore}>{children}</StoreContext.Provider>;
+export const StoreProvider = ({ children }: StoreProviderProps) => {
+  const [isLoaded, setIsLoaded] = useState<boolean>(false);
+
+  //Checks the availability of the local storage and persists the root store data if available.
+  const checkLocalStorageAvailability = async () => {
+    try {
+      if (window.localStorage) {
+        persist('all', rootStore, {
+          storage: localStorage,
+          jsonify: true,
+        }).then(() => {
+          rootStore.afterCreate();
+          setIsLoaded(true);
+        });
+      }
+    } catch (error) {
+      setIsLoaded(true);
+    }
+  };
+
+  useEffect(() => {
+		checkLocalStorageAvailability();
+	}, []);
+
+  return isLoaded ? <StoreContext.Provider value={rootStore}>{children}</StoreContext.Provider> : <></>;
+};
 
 export const useStore = () => useContext(StoreContext) as RootStoreType;
--- src/stores/root.store.ts
+++ src/stores/root.store.ts
@@ -1,17 +1,27 @@
-import { Instance, types } from "mobx-state-tree";
+import { Instance, applySnapshot, getSnapshot, types } from "mobx-state-tree";
 import { UserStore, userStore } from './user.store';
 
 export type RootStoreType = Instance<typeof RootStore>;
 
-export const RootStore = types.model("RootStore", {
-  user: UserStore,
-});
+export const RootStore = types
+  .model("RootStore", {
+    user: UserStore,
+  })
+  .actions((self) => {
+    let initialState = {};
+    return {
+      afterCreate: () => {
+        initialState = getSnapshot(self);
+      },
+      reset: () => {
+        applySnapshot(self, initialState);
+      },
+    };
+  });
 
 export const createStore = () =>
   RootStore.create({
     user: userStore,
   });
-
-//TODO: Add a persist to save the store in local storage
 
 export const rootStore = createStore();
